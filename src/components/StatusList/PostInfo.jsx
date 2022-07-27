102f46e8a322df743decfab6a7fb7c9f62951e4d
Patma12 <107095346+Patma12@users.noreply.github.com>
2022-07-27 14:30:43 +0700

    add StatusList StatusCard

---
+++ src/components/StatusList/PostInfo.jsx
@@ -0,0 +1,28 @@
+const cardInfo =  [
+    {
+        id: 1,
+        img: "../src/assets/imageCard.png",
+        activity: "yoca",
+        durationTime: "1hr 5s",
+        textPost: "วันนี้ไม่อยากวิ่งแล้วววววววว",
+        datePost: "TUE, 20 AUG 2020"
+    },
+    {
+        id: 2,
+        img: "../src/assets/yoca.jpg",
+        activity: "yoca2",
+        durationTime: "1hr 5s",
+        textPost: "โยคะได้อีกยาวววว",
+        datePost: "WED, 21 AUG 2020"
+    },
+    {
+        id: 3,
+        img: "../src/assets/run.jpg",
+        activity: "yoca3",
+        durationTime: "1hr 5s",
+        textPost: "ออกกำลังกายเหนือยน่ะแต่ก็ต้องทำ",
+        datePost: "WED, 21 AUG 2020"
+    }
+]
+
+export default cardInfo;
