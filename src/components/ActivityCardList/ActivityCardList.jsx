c007f785dab0154b54fc73d2bdb26e4c194492fa
Sukit Bunsiri <100616257+Neolization@users.noreply.github.com>
2022-08-02 11:46:35 +0700

    wip hotfix

--- src/components/ActivityCardList/ActivityCardList.jsx
+++ src/components/ActivityCardList/ActivityCardList.jsx
@@ -6,13 +6,23 @@ import activityInfo from './ActivityInfo';
 
 
 function CreateActivity(activityInfo) {
-    return ( <ActivityCard 
+
+  // hotfix
+
+  function VideoIdParse(url) {
+  let re = /(https?:\/\/)?(((m|www)\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
+  let VideoId = url.match(re)[7];
+  }
+
+    return ( 
+    <ActivityCard 
         key={activityInfo.id}
         img={activityInfo.img}
-        video={activityInfo.video}
+        video=
         activity={activityInfo.activity}
         durationTime={activityInfo.durationTime}
-        />)
+        />
+        )
 }
 
 function ActivityCardList() {
