335a8cc8839fc355263cca4ccdab898cefffe09d
Tenniz <veerakamjorn.r@gmail.com>
2022-08-02 09:51:38 +0700

    ีupdte navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,1 +1,12 @@
-import something
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className="navbar">
+      <a className="navbar-home" href="#"><img src='./src/assets/Add.png' /></a>
+      <a className="navbar-logo" href="#"><img src='./src/assets/Flog.png' /></a>
+      <a className="navbar-profile" href="#"><img src='./src/assets/Profile Pic.png' /></a>
+    </div>
+  )
+}
+export default Navbar;
