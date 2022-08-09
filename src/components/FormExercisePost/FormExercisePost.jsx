076206b4332aaec045efbe1439bafd2f6bbdf76b
Patma12 <107095346+Patma12@users.noreply.github.com>
2022-08-09 15:22:34 +0700

    update FormExercisePost

--- src/components/FormExercisePost/FormExercisePost.jsx
+++ src/components/FormExercisePost/FormExercisePost.jsx
@@ -1,29 +1,74 @@
 import React, { useState } from "react";
 import './FormExercisePost.css'
 
-function CreatPost() {
-    return (
-        <div className="formEx">
-            <h1>What's on your mind?</h1>
-            <div className="min">
-                <p className="left">WARM UP BODY</p>
-                <p className="right">10MINS</p>
+
+
+function CreatePost(props) {
+
+  const [makePost, setMakePost] = useState({
+    activity: "Activity2",
+    durationTime: "durationTime2",
+    textPost:"",
+    datePost:"datePost",
+   
+   
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setMakePost(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
+  const [img, setImg] = useState('./111.png')
+  function onImageChange(event) {
+    const [file] = event.target.files;
+    setImg(URL.createObjectURL(file));
+  }
+
+//   function submitPost(event) {
+//     const date = new Date();
+//     props.onAdd(makePost);
+//     setMakePost({
+//       activity: "",
+//       durationTime: "",
+//       textPost:"",
+//       datePost: date.toLocaleTimeString(),
+//       img:"./111.png"
+//     });
+//     event.preventDefault();
+//   }
+
+  
+
+
+ 
+
+  return (
+    <div className='form-post'>
+        <h1>What's on your mind? </h1>
+
+        <div className="post-form-middle">
+            <p onChange={handleChange} > {makePost.activity} {makePost.durationTime}</p>
+            <textarea className="form-texta" onChange={handleChange} name="textPost" id="" cols="30" rows="10" placeholder="What is your mind?" value={makePost.textPost}></textarea>
+
+            <div className="image-post input-post" >  
+                <label htmlFor="file-input" >
+                    <img className="" src={img} alt="" />
+                </label>
+                <input name="img" type="file" id="file-input" onChange={onImageChange} />
             </div>
-
-
-            <textarea
-                name="content"
-                placeholder="Take a note..."
-                rows="3"
-            />
-            <input
-                name="title"
-                placeholder="Title"
-                type='file'
-            />
-
-            <button className='post'>Post</button>
-
-        </div>);
+        </div>
+        
+        
+        <button className="post-button"  >POST</button>
+    </div>
+  )
 }
-export default CreatPost;
+
+export default CreatePost;
