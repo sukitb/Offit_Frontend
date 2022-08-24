220ee48981d4137734e57c59f68d738ba2d9428c
Sukit Bunsiri <100616257+Neolization@users.noreply.github.com>
2022-08-25 02:57:26 +0700

    change base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:8080/users',
+  baseURL: 'https://offit-backend.vercel.app/users',
   withCredentials: true,
   validateStatus: () => true
 });
